702b31d927991d6eaf217cab6472c1fa1ad55f9e
vothedien <theneid28@gmail.com>
2025-11-19 02:56:43 +0700

    av

--- src/app/api/vnpay/ipn/route.ts
+++ src/app/api/vnpay/ipn/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SUPABASE_ANON_KEY!
 );
 
 function sortObject(obj: Record<string, string>) {
