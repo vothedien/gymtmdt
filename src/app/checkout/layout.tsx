31a0a3d3ebd0e29f99755f6053647bf9183636dd
vothedien <theneid28@gmail.com>
2025-11-18 17:03:05 +0700

    loading

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Suspense fallback={<div>Đang tải...</div>}>
+    <Suspense fallback={<div>Đang tải trang checkout...</div>}>
       {children}
     </Suspense>
   );
 }
+
